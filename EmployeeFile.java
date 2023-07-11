8f34a6fc67ad9f9e1c611768f76c87e056733a42
BhatRajendra <120010518+BhatRajendra@users.noreply.github.com>
2023-07-11 10:15:26 +0530

    Update EmployeeFile.java

--- EmployeeFile.java
+++ EmployeeFile.java
@@ -55,11 +55,11 @@ try
 FileWriter fw=new FileWriter(fileobj.getAbsoluteFile(),true);
 System.out.println("\n NAME : " +f1.getText() +"\n" +"ID : "
 
-+f2.getText() +"\n" +"DOJ :" +f3.getText() +"\n" +"DOB"+f4.getText() +"\n");
++f2.getText() +"\n" +"DOJ :" +f3.getText() +"\n" +"DOB: "+f4.getText() +"\n");
 
-fw.write("Name:" +f1.getText() +"\n"+"ID:"+f2.getText()
+fw.write("Name: " +f1.getText() +"\n"+"ID: "+f2.getText()
 
-+"\n" +"DOJ:"+f3.getText() +"\n" +"DOB"+f4.getText() +"\n");
++"\n" +"DOJ: "+f3.getText() +"\n" +"DOB: "+f4.getText() +"\n");
 
 fw.close();
 }
